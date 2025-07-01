3f9777499a7de7ea0292969015f6c4c1db036518
Tarek <Tareksaeed11111@gmail.com>
2025-07-01 17:29:44 +0300

    Add localization support and initial language files for Arabic and English

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:evently_app/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,9 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: HomeScreen.routeName, routes: {});
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      locale: Locale('en'),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+    );
   }
 }
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,6 +7,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.arrow_back_ios, color: Colors.white),
+        title: Text(AppLocalizations.of(context)!.language, style: TextStyle(color: Colors.white)),
+      ),
+    );
   }
 }
